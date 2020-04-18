cf975659d1e4b5a3832494b15fc59fd51f1a6a21
AName <workruft@gmail.com>
2020-04-18 16:40:59 -0400

    Got custom geometry with lighting to work!

--- Client/index.js
+++ Client/index.js
@@ -4,62 +4,88 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+//Directions:
+//  -Z    /\+Y
+//-X  +X  ||
+//  +Z    \/-Y
+
+let fieldOfView = 75;
+let aspectRatio = window.innerWidth / window.innerHeight;
+let near = 0.1;
+let far = 1000;
+let camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
 camera.position.set(0, 0, 30);
 camera.lookAt(0, 0, 0);
 
-function createCube() {
-    return new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 'green' }));
+let cubeGeometry = new THREE.Geometry();
+//Corners:
+//  6----7
+// /|   /|
+//2----3 |
+//| |  | |
+//| 4--|-5
+//|/   |/
+//0----1
+cubeGeometry.vertices.push(
+    //In order from 0-7:
+    new THREE.Vector3(-1, -1,  1),
+    new THREE.Vector3( 1, -1,  1),
+    new THREE.Vector3(-1,  1,  1),
+    new THREE.Vector3( 1,  1,  1),
+    new THREE.Vector3(-1, -1, -1),
+    new THREE.Vector3( 1, -1, -1),
+    new THREE.Vector3(-1,  1, -1),
+    new THREE.Vector3( 1,  1, -1)
+);
+//Faces. Must be in counter-clockwise direction to be facing outside.
+//Each integer is merely referencing a corner vertex.
+cubeGeometry.faces.push(
+    //Front.
+    new THREE.Face3(0, 3, 2),
+    new THREE.Face3(0, 1, 3),
+    //Right.
+    new THREE.Face3(1, 7, 3),
+    new THREE.Face3(1, 5, 7),
+    //Back.
+    new THREE.Face3(5, 6, 7),
+    new THREE.Face3(5, 4, 6),
+    //Left.
+    new THREE.Face3(4, 2, 6),
+    new THREE.Face3(4, 0, 2),
+    //Top.
+    new THREE.Face3(2, 7, 6),
+    new THREE.Face3(2, 3, 7),
+    //Bottom.
+    new THREE.Face3(4, 1, 0),
+    new THREE.Face3(4, 5, 1),
+);
+//For lighting.
+cubeGeometry.computeFaceNormals();
+function createCustomCube() {
+    return new THREE.Mesh(cubeGeometry, new THREE.MeshPhongMaterial({ color: 'green' }));
 }
-let cubes = [];
+let customCubes = [];
 for (let x = -5; x < 5; ++x) {
     for (let y = -5; y < 5; ++y) {
-        let cube = createCube();
-        cube.position.set(x * 2, y * 2, 0);
-        cubes.push(cube);
-        scene.add(cube);
+        let customCube = createCustomCube();
+        customCube.position.set(x * 2, y * 2, 0);
+        customCubes.push(customCube);
+        scene.add(customCube);
     }
 }
 
-// function createLines() {
-//     return new THREE.Line(new THREE.BufferGeometry().setFromPoints([
-//         new THREE.Vector3(-10, 0, 1),
-//         new THREE.Vector3(0, 10, 1),
-//         new THREE.Vector3(10, 0, 1)
-//     ]), new THREE.LineBasicMaterial({ color: 'blue' }));
-// }
-// let lines = createLines();
-// scene.add(lines);
+let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+scene.add(ambientLight);
+let directionalLight = new THREE.DirectionalLight('white', 0.5);
+directionalLight.position.set(100, 100, 100);
+scene.add(directionalLight);
 
-// function createGround() {
-//     let groundGeo = new THREE.PlaneBufferGeometry(10000, 10000);
-//     let groundMat = new THREE.MeshLambertMaterial({ color: 'white' });
-//     groundMat.color.setHSL(0.095, 1, 0.75);
-//     let ground = new THREE.Mesh(groundGeo, groundMat);
-//     ground.position.y = -33;
-//     ground.position.x = -Math.PI / 2;
-//     ground.position.z = -5;
-//     ground.receiveShadow = true;
-//     return ground;
-// }
-// let ground = createGround();
-// scene.add(ground);
-
-function createHemisphereLight() {
-    let hemiLight = new THREE.HemisphereLight('white', 'white', 0.6);
-    hemiLight.color.setHSL(0.6, 1, 0.6);
-    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
-    hemiLight.position.set(0, 50, 0);
-    return hemiLight;
-}
-let hemiLight = createHemisphereLight();
-scene.add(hemiLight);
 
 function animate() {
     requestAnimationFrame(animate);
-    for (let i = 0; i < cubes.length; ++i) {
-        cubes[i].rotation.x += 0.01;
-        cubes[i].rotation.y += 0.01;
+    for (let i = 0; i < customCubes.length; ++i) {
+        customCubes[i].rotation.x += 0.01;
+        customCubes[i].rotation.y += 0.01;
     }
     renderer.render(scene, camera);
 }
--- Miscellaneous/miscellaneous.js
+++ Miscellaneous/miscellaneous.js
@@ -11,3 +11,28 @@ const seen = new WeakSet();
     };
 };
 JSON.stringify(request, getCircularReplacer());
+
+
+
+function createCube() {
+    return new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 'green' }));
+}
+let cubes = [];
+for (let x = -5; x < 5; ++x) {
+    for (let y = -5; y < 5; ++y) {
+        let cube = createCube();
+        cube.position.set(x * 2, y * 2, 0);
+        cubes.push(cube);
+        scene.add(cube);
+    }
+}
+
+function createLines() {
+    return new THREE.Line(new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(-10, 0, 1),
+        new THREE.Vector3(0, 10, 1),
+        new THREE.Vector3(10, 0, 1)
+    ]), new THREE.LineBasicMaterial({ color: 'blue' }));
+}
+let lines = createLines();
+scene.add(lines);
